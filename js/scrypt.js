0398455022fb94d81bb006a9fa3ed1aa5da12f62
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-03-20 17:07:14 +0100

    fix eta

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,19 +1,17 @@
 const km = prompt('Inserire numero km da percorrere');
 
-const età = prompt('Inserire età del passeggero');
+const eta = prompt('Inserire età del passeggero');
 
 const prezzoKm = 0.21;
 
 const parziale = prezzoKm * km;
 
-let totale = "";
+let totale = parziale;
 
-if (età < 18){
+if (eta < 18){
   totale = ( parziale - ((parziale / 100) * 20 ));
-}else if (età > 65){
+}else if (eta > 64){
   totale = ( parziale - ((parziale / 100) * 40 ));
-}else{
-  totale = parziale;
 }
 
 let message = `
@@ -22,4 +20,4 @@ Il costo totale del viaggio sarà di ${totale.toFixed(2)}€
 
 document.getElementById('output').innerHTML = message;
 
-console.log(km, età, parziale, totale);
+console.log(km, eta, parziale, totale);
