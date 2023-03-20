258ee48265b39e41ff8358ab74d3a4cb4040b52b
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-03-20 16:25:55 +0100

    add prezzoKm, parziale, totale

--- js/scrypt.js
+++ js/scrypt.js
@@ -2,4 +2,20 @@ const km = prompt('Inserire numero km da percorrere');
 
 const età = prompt('Inserire età del passeggero');
 
-console.log(km, età);
+const prezzoKm = 0.21;
+
+const parziale = prezzoKm * km;
+
+let totale = "";
+
+if (età < 18){
+  totale = ( parziale - ((parziale / 100) * 20 ));
+}else if (età > 65){
+  totale = ( parziale - ((parziale / 100) * 40 ));
+}else{
+  totale = parziale;
+}
+
+
+
+console.log(km, età, parziale, totale);
